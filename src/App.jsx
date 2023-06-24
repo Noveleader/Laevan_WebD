40c9ea22dcfaa4214efa4932ca4e50a8cfb3e168
Vedant Vatsa <26vedu@gmail.com>
2023-06-25 00:23:15 +0530

    New Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,85 @@
-// We code react app here
-function Some(){
-    return (
-        <>
-  <title>Login Page</title>
-  <link rel="stylesheet" type="text/css" href="styles.css" />
-  <div class="container">
-    <div className="image-container">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdVB2K5x6ikhS13sqXBQzcPkET2lcuHB615CCI-OvTicFSOU5ckxbUjt7CG3WXHSFftXaFu-1WL_8&usqp=CAU&ec=48600112" alt="Image" />
-    </div>
-    <div className="login-container">
-        <h2>Login</h2>
-        <form>
-        <div className="input-submit">
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" required="" />
-        </div>
-        <div className="input-submit">
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" required="" />
-        </div>
-        <div className="input-submit">
-            <input type="submit-login" defaultValue="Login" />
-        </div>
-        <div className="input-submit">
-            <input type="submit-register" defaultValue="Register" />
-        </div>
-        </form>
-    </div>
-  </div>
-</>
-    );
-    }
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-export default function main() {
+function Register() {
     return (
-        <>
-        < Some/>
-        </>
+      <>
+        <div className="register-container">
+          <title>Registration Form</title>
+          <h2 className="form-heading">Registration Form</h2>
+          <form>
+            <div className="reg">
+              
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" placeholder="Enter your name" />
+              <label htmlFor="UserName">UserName:</label>
+              <input
+                type="text"
+                id="username"
+                name="UserName"
+                placeholder="Enter your E-mail address"
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+              />
+              <div className="input-submit">
+                <Link to="/some">
+                  <button>Register</button>
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
+      </>
     );
+  }
+  
+  
+
+function Some() {
+  return (
+    <>
+      <title>Login Page</title>
+      <link rel="stylesheet" type="text/css" href="styles.css" />
+      <div className="container">
+        <div className="login-container">
+          <h2>Login</h2>
+          <form>
+            <div className="input-submit">
+              <label htmlFor="username">Username:</label>
+              <input type="text" id="username" name="username" required="" />
+            </div>
+            <div className="input-submit">
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" required="" />
+            </div>
+            <div className="input-submit">
+              <button type="submit" className="submit-login">Login</button>
+            </div>
+            <div className="input-submit">
+              <Link to="/register">
+                <button>Register</button>
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/some" element={<Some />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
